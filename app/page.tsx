b04e939e7d2e6ac1c5c7fe25dd162740aadeb186
honorekalisa <h.kalisa@alustudent.com>
2023-11-25 13:53:15 +0200

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import AuthButton from "../components/AuthButton";
+import Header from "@/components/Header";
+
+export default async function Index() {
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+          <h1>TechPath Navigator</h1>
+          <AuthButton />
+        </div>
+      </nav>
+      <main className="w-full max-w-4xl flex flex-col gap-12 p-3">
+        <Header />
+      </main>
+      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+        <p>
+          Powered by{" "}
+          <a
+            href="https://supabase.com/"
+            target="_blank"
+            className="font-bold hover:underline"
+            rel="noreferrer"
+          >
+            Supabase
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+}
